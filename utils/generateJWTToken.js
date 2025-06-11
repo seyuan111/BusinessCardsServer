32a06aae2a86a3b5563b350d0b7622fd91436338
Sean Yuan <ysean54@yahoo.com>
2025-06-10 21:10:11 -0400

    made changes

--- utils/generateJWTToken.js
+++ utils/generateJWTToken.js
@@ -8,7 +8,7 @@ export const generateJWTToken = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 1000
+        maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
     return token
