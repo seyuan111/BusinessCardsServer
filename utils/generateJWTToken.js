38c0d78bf49c92e5b1c4da1327630d214973811a
Sean Yuan <ysean54@yahoo.com>
2025-09-16 21:52:47 -0400

    changed the sameSite from generateJWTToken

--- utils/generateJWTToken.js
+++ utils/generateJWTToken.js
@@ -4,10 +4,11 @@ export const generateJWTToken = (res, userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: "7d"
     })
+    
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production", // true in production
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" for cross-origin in production
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
